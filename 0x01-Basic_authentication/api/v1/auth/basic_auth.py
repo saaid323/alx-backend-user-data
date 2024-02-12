51892fbfaf2b8b7a1acc9378e4fa5a3bad7bd14f
saaid323 <saaid2538@gmail.com>
2024-02-12 20:35:58 +0000

    added basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basiid Authentication module"""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -16,3 +18,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        return decode.decode('ascii')

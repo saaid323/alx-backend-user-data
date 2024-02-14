8320a4ba3e3919c652e5d23b31e7b1df86d261c4
saaid323 <saaid2538@gmail.com>
2024-02-14 14:58:44 +0000

    added session_auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Module to manage the API authentication"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -35,3 +36,10 @@ class Auth:
         """returns None"""
         if request is None:
             return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        cookie = os.getenv('SESSION_NAME')
+        return request.cookies.get(cookie)

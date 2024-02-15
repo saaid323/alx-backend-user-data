cde61bde4650ce7269ad719157a7105557cf0e51
saaid323 <saaid2538@gmail.com>
2024-02-15 16:58:04 +0000

    added models/user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -9,5 +9,5 @@ class UserSession(Base):
     def __init__(self, *args: list, **kwargs: dict):
         """init method"""
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id: str = kwargs.get('user_id')
+        self.session_id: str = kwargs.get('session_id')

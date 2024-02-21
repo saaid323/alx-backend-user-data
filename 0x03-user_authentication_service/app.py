2af57580c9262e6f940c34cbabe8edd82944d098
saaid323 <saaid2538@gmail.com>
2024-02-21 11:11:03 +0000

    updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,15 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', strict_slashes=False, methods=['GET'])
+def profile():
+    '''finds user email'''
+    cookie = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(cookie)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")

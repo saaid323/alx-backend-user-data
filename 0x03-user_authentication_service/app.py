d414a310a484c242f5af838667e2598a28238a7d
saaid323 <saaid2538@gmail.com>
2024-02-21 12:09:37 +0000

    updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,16 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', strict_slashes=False, methods=['POST'])
+def get_reset_password_token():
+    '''reset password token'''
+    email = request.form.get('email')
+    try:
+        user = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": user})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")

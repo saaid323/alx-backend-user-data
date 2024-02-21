4ea3568fbdf4d8d14832a30cd70810d69b618830
saaid323 <saaid2538@gmail.com>
2024-02-21 11:01:14 +0000

    updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,7 @@ def logout():
     if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect(url_for('/'))
+    return redirect('/')
 
 
 if __name__ == '__main__':
